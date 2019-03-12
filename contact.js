c40866655a5491433c98ea56e19e20195b1bdf8f
Mohamed B Hussein <36186497+mjBaker210@users.noreply.github.com>
2019-03-11 21:20:49 -0500

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,51 @@
+$(document).ready(Function() {
+	function init() {
+	    if (localStorage["name"]) {
+			 $('#name').val(localStorage["name"]);
+		}
+	     if (localStorage["email"]) {
+			 $('#email').val(localStorage["email"]);
+		 }
+	     if (localStorage["message"]) {
+			 $('#message').val(localStorage["message"]);
+		 }
+    
+   }
+    init();
+});
+
+$('.stored').change(function()) {
+			localStorage[$(this).attr('name')] = $(this).val();
+														   
+});
+					
+
+	        
+   
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  }
